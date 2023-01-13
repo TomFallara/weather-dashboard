6ade9c42aadcaa9076465cdfce317292ab17ff19
Impp18 <tdfallara@gmail.com>
2023-01-13 11:00:19 -0500

    fixed button logic, updated button style

--- assests/script.js
+++ assests/script.js
@@ -4,7 +4,6 @@ var cityName = "";
 var cityInput = document.getElementById('cityInput')
 var searchBtn = document.getElementById('searchBtn');
 var chosenCityName = document.getElementById('currentName');
-var chosenCityIcon = document.getElementById('currentIcon')
 var chosenCityTemp = document.getElementById('currentTemp');
 var chosenCityHumidity = document.getElementById('currentHumidity');
 var chosenCityWindSpeed = document.getElementById('currentWindSpeed');
@@ -25,16 +24,17 @@ var displayClock = function () {
 var searchedCities = JSON.parse(localStorage.getItem('searchedCities')) ?? [];
 
 //display old searches
-var displayOld = function() {
+var displayOld = function () {
   for(i=0; i<searchedCities.length; i++) {
     var cityBtn = document.createElement('button')
     cityBtn.textContent = searchedCities[i].city
+    cityBtn.setAttribute('style', 'display: block')
+    cityBtn.setAttribute('style', 'width: 100%')
     cityList.appendChild(cityBtn)
     cityBtn.addEventListener('click', function(event) {
       getLatLong(this.textContent)})
     }
-  }
-  
+}
 //fetch weather data for today
 function fetchWeather(lat, long, cityName) {
   var weatherApiURL = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=' + lat + '&lon=' + long + '&appid=' + apiKey;
@@ -47,13 +47,29 @@ function fetchWeather(lat, long, cityName) {
     chosenCityTemp.textContent = `Temp: ${data.list[0].main.temp.toFixed(0)}°F`
     chosenCityHumidity.textContent = `Humidity: ${data.list[0].main.humidity}%`
     chosenCityWindSpeed.textContent = `Wind Speed: ${data.list[0].wind.speed.toFixed(0)}mph`
-    chosenCityIcon 
+    var chosenCityIcon = document.createElement('img')
+    chosenCityIcon.setAttribute('src', 'https://openweathermap.org/img/wn/' + data.list[0].weather[0].icon + '.png')
+    chosenCityIcon.setAttribute('alt', 'icon that depicts the current weather conditions in the city chosen')
+    chosenCityName.appendChild(chosenCityIcon)
   })
 }
 
 //fetch 5-day forcast and create cards
 function fiveDayForecast(){
-
+  var weatherApiURL = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=' + lat + '&lon=' + long + '&appid=' + apiKey;
+  fetch(weatherApiURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function(data){
+    var fiveDayCard = document.createElement('div');
+    var fiveDayCardDate = document.createElement('h3');
+    var fiveDayCardIcon = document.createElement('img');
+    var fiveDayCardTemp = document.createElement('p');
+    var fiveDayCardHumiditiy = document.createElement('p');
+    var fiveDayCardWindSpeed = document.createElement('p');
+    fi
+  })
 }
 
 //use Geocode to fetch coordinates for weather API
@@ -74,10 +90,17 @@ function getLatLong(name) {
       long: long,
     };
     //make sure no doubles exist
-    var noDuplicates = searchedCities.findIndex(object => object.name === addedCity.name)
+    var noDuplicates = searchedCities.findIndex(object => object.city === addedCity.city)
     if(noDuplicates === -1){
+    //if not a duplicated, add to the searchedCities array, and make a new button
       searchedCities.push(addedCity)
-      displayOld();
+      var newBtn = document.createElement('button')
+      newBtn.textContent = addedCity.city
+      newBtn.setAttribute('style', 'display: block')
+      newBtn.setAttribute('style', 'width: 100%')
+      cityList.appendChild(newBtn)
+      newBtn.addEventListener('click', function(event) {
+        getLatLong(this.textContent)})
     }
     
     //save to local storage
